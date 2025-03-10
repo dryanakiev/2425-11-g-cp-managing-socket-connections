a69b3cf964de1717335899c0f66bfe771e26a26b
dryanakiev <dyanakiev@codingburgas.bg>
2025-03-10 16:14:44 +0200

    Separate html in separate files

--- PythonHttpServer/Server.py
+++ PythonHttpServer/Server.py
@@ -10,19 +10,50 @@ print(f"Server running on http://{ip_address}:{port}")
 
 while True:
     client_connection, client_address = server_socket.accept()
-    request = client_connection.recv(1024).decode()
+    request = client_connection.recv(1024).decode().strip()
+
+    if not request:
+        client_connection.close()
+        continue
+
     print(f"Request received:\n{request}")
 
-    # Define HTML response
-    response = """HTTP/1.1 200 OK\nContent-Type: text/html\n\n
-    <html>
-    <head><title>Simple Python HTTP Server</title></head>
-    <body>
-        <h1>Hello, World!</h1>
-        <p>This is a simple HTTP server in Python.</p>
-    </body>
-    </html>
-    """
+    request_lines = request.split("\n")
+
+    if len(request_lines) > 0 and " " in request_lines[0]:
+        path = request_lines[0].split(" ")[1]
+    else:
+        path = "/"
+
+    if path == "/":
+        file_name = "index.html"
+        directory = "./pages/"
+        content_type = "text/html"
+
+    elif path == "/about":
+        file_name = "about.html"
+        directory = "./pages/"
+        content_type = "text/html"
+
+    elif path == "/contact":
+        file_name = "contact.html"
+        directory = "./pages/"
+        content_type = "text/html"
+
+    elif path == "/register":
+        file_name = "register.html"
+        directory = "./pages/"
+        content_type = "text/html"
+
+    elif path.startswith("/styles/"):
+        file_name = path.lstrip("/")
+        directory = "./"
+        content_type = "text/css"
+    else:
+        file_name = None
+
+    file_content = open(f'{directory}{file_name}', 'r').read()
+    response = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\n\r\n{file_content}"
 
     client_connection.sendall(response.encode())
     client_connection.close()
